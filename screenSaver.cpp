f6084cd598957d0cfc480f4df4fb96ec76d10ce0
alvinjwong <ajwong@uab.edu>
2024-10-04 22:00:35 -0500

    maybe made custommaker

--- screenSaver.cpp
+++ screenSaver.cpp
@@ -91,3 +91,19 @@ void ColorChangingSaver::update(float deltaTime, int screenWidth, int screenHeig
   }
 
 }
+
+CustomSaver::CustomSaver(float someFloat, sf::Vector2f someVector, float someOtherFloat) :
+ScreenSaver(someFloat, someVector, sf::Color::Yellow), somefloat(someFloat) someVector(someVector),
+someOtherFloat(someOtherFloat) {}
+
+void CustomSaver::update(float deltaTime, int screenWidth, int screenHeight) {
+  float angle = deltaTime * someOtherFloat * .05;
+  float radius = 100.0f;
+  float centerX = screenWidth / 2.0;
+  float centerY = screenHeight / 2.0;
+
+  float x = centerX + radius * std::cos(angle);
+  float y = centerY + radius * std::sin(angle);
+
+  getShape().setPosition(x, y);
+}
