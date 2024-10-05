f34b09e9b89b7fd51aec7d97569070564cde95e9
alvinjwong <ajwong@uab.edu>
2024-10-04 23:07:48 -0500

    ugh

--- screenSaver.cpp
+++ screenSaver.cpp
@@ -96,8 +96,8 @@ ScreenSaver(someFloat, someVector, sf::Color::Yellow), someFloat(someFloat), som
 someOtherFloat(someOtherFloat) {}
 
 void CustomSaver::update(float deltaTime, int screenWidth, int screenHeight) {
-  float angle = deltaTime * someOtherFloat * .001f;
-  float radius = 5000.0f;
+  float angle = deltaTime * someOtherFloat * .1f;
+  float radius = 100.0f;
   float centerX = screenWidth / 2.0f;
   float centerY = screenHeight / 2.0f;
 
